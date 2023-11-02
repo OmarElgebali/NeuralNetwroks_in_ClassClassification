7f6c0a8a1e1ff4d320de2d6047fa4583b9ca2d1d
Emad Mahmoud <emadmahmoudelthan2020@gmail.com>
2023-11-02 15:51:44 +0200

    select_feature_columns

--- Perceptron.py
+++ Perceptron.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-# Read the CSV file into a DataFrame
-csv_file_path = 'Dry_Bean_Dataset.csv'
+csv_file_path = 'cropped_file_P.csv'
+
 df = pd.read_csv(csv_file_path)
 
-# Crop the first 101 rows
-cropped_df = df.iloc[:100]
+# Extract the first two columns and the last column
+selected_columns = df.iloc[:, [0, 1, -1]]
 
-# Save the cropped DataFrame to a new CSV file
-output_csv_file_path = 'cropped_file_P.csv'
-cropped_df.to_csv(output_csv_file_path, index=False)
+# Save the selected columns to a new CSV file
+output_csv_file_path = 'selected_columns.csv'
+selected_columns.to_csv(output_csv_file_path, index=False)
+
+
