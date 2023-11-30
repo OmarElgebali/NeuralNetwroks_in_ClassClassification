f365bce26cc8dde33dd42847a5626fbfaee94fb9
Omar Elgebali <99504626+OmarElgebali@users.noreply.github.com>
2023-11-30 19:56:23 +0200

    Update Core.py

--- Task_2/Core.py
+++ Task_2/Core.py
@@ -10,15 +10,18 @@ def preprocessing(activation_function, is_bias):
 
 
 def fit(activation_function, epochs, eta, bias, layers, neurons_list):
-    # for _ in epochs:
-    for xs in x_train:
-        generateWeights(neurons_list)
-        ys, ws = feed_forward(xs, activation_function)
-        error_signal = back_propagation(ys, y_train, ws)
-        network_xs = ys[:(len(ys) - 1)]
-        network_xs.insert(0, xs)
-        print_list_of_lists(network_xs, 'network_xs')
-        uws = updateWeights(error_signal, eta, network_xs)
+    generateWeights(neurons_list)
+    for _ in epochs:
+        for xs in x_train:
+            ys, ws = feed_forward(xs, activation_function)
+            error_signal = back_propagation(ys, y_train, ws)
+            network_xs = ys[:(len(ys) - 1)]
+            for i in range(len(network_xs)):
+                network_xs[i].insert(0, bias)
+            network_xs.insert(0, xs)
+            print_list_of_lists(network_xs, 'network_xs')
+            uws = updateWeights(error_signal, eta, network_xs)
+
 
 
 # def predict(xs):
