e5909b2cbdf3cf99959c0f335023cc14faa84612
Mohamed Osama <87633359+MohamedOsama911@users.noreply.github.com>
2023-11-30 19:26:31 +0200

    fix the list of Xs on update weight

--- Task_2/Kernel.py
+++ Task_2/Kernel.py
@@ -66,37 +66,65 @@
 # [  [[],[],[],[]],        [[],[],[],[]],          [[],[],[],[]]
 # [  [[],[],[],[]],        [[],[],[],[]],          [[],[],[],[]]
 # ]
+# weights = \
+#     [
+#         [[-.3, .21, .15], [.25, -.4, .1]],
+#         [[-.4, -.2, .3]]
+#     ]
+#
+# errors = [
+#     [.005, -.007],
+#     [-.102]
+# ]
+# Xs = \
+#     [
+#         [[1, 0, 0], [1, 0, 0]],
+#         [[1, 0.43, 0.56]]
+#     ]
+#
+# newWeights = []
+
+
+# DON'T DELETE THIS!!!!  will use it later...
+# def updateWeights(weights, errors, learningRate, Xs):
+#     for layer, error_list in enumerate(errors):
+#         for neuron, neuron_error in enumerate(error_list):
+#             for i in range(len(weights[layer][neuron])):
+#                 weights[layer][neuron][i] = weights[layer][neuron][i] + \
+#                                             learningRate * neuron_error * Xs[layer][neuron][i]
+#     return weights
+
 weights = \
     [
-        [[-.3, .21, .15], [.25, -.4, .1]],
-        [[-.4, -.2, .3]]
+        [[-.4, .2, .4, -.5], [.2, -.3, .1, .2]],
+        [[.1, -.3, -.2]]
     ]
 
 errors = [
-    [.005, -.007],
-    [-.102]
+    [-.0087, .0065],
+    [.1311]
 ]
 Xs = \
     [
-        [[1, 0, 0], [1, 0, 0]],
-        [[1, 0.43, 0.56]]
+        [1, 1, 0, 1]
+        , [1, 0.332, 0.525]
     ]
-
-newWeights = []
-
-print("")
 
 
 def updateWeights(weights, errors, learningRate, Xs):
     for layer, error_list in enumerate(errors):
         for neuron, neuron_error in enumerate(error_list):
             for i in range(len(weights[layer][neuron])):
-                weights[layer][neuron][i] = weights[layer][neuron][i] + learningRate * neuron_error * Xs[layer][neuron][
-                    i]
+                weights[layer][neuron][i] = weights[layer][neuron][i] + \
+                                            learningRate * neuron_error * Xs[layer][i]
     return weights
 
 
 # learning rate=.01
-print(updateWeights(weights, errors, 0.01, Xs))
-# output
+print(updateWeights(weights, errors, 0.9, Xs))
+
+# first output
 # [[[-0.29995, 0.21, 0.15], [0.24993, -0.4, 0.1]], [[-0.40102000000000004, -0.20043860000000002, 0.2994288]]]
+
+# second output
+# [[[-0.40783, 0.19217, 0.4, -0.50783], [0.20585, -0.29414999999999997, 0.1, 0.20585]], [[0.21799000000000002, -0.26082732, -0.13805525000000002]]]
